28eca47c0b51186359e838279546f3e550c3b9d7
Velislava <viliasparuhova02@gmail.com>
2023-01-10 18:51:21 +0200

    Add

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!--swiper css link -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />">
+
+    <!--custom css file link-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <!-- header section starts -->
+    <section class="header">
+        <a href="home.php" class="logo">TravelExpress</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Packages</a>
+            <a href="book.php">Book</a>
+        </nav>
+    </section>
+
+    <!-- header section ends -->
+
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <!--custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
